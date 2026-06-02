205b0f9ed56ffc3a72d5ca15956cb58ef62d284d
Sávio Romário <savioromario10@gmail.com>
2026-06-02 17:42:45 -0300

    first commit

---
+++ src/test/java/io/savioromario10/vendas/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.savioromario10.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
